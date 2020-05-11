2d78aa55195335709dd60283f75c12a094e89f20
Mizuho <mizuho128@gmail.com>
2020-05-12 00:21:14 +0900

    Julia ch05, TLN2 needs batch_size free impl

---
+++ julia/5_backpropagation.jl
@@ -0,0 +1,440 @@
+# %% 5.4 単純なレイヤの実装
+module BP
+  export MulLayer
+  mutable struct MulLayer
+    x::Number
+    y::Number
+  end
+  function MulLayer()
+    return MulLayer(nothing, nothing)
+  end
+  function forward(self::MulLayer, x, y)
+    self.x = x
+    self.y = y
+    out = x * y
+
+    return out
+  end
+  function backward(self::MulLayer, dout)
+    dx = dout * self.y
+    dy = dout * self.x
+
+    return dx, dy
+end
+
+  export AddLayer
+  mutable struct AddLayer
+    x::Number
+    y::Number
+  end
+  function AddLayer()
+    return AddLayer(0, 0)
+  end
+  function forward(self::AddLayer, x, y)
+    self.x = x
+    self.y = y
+    out = x + y
+
+    return out
+  end
+  function backward(self::AddLayer, dout)
+    dx = dout * 1
+    dy = dout * 1
+
+    return dx, dy
+  end
+
+  export ReluLayer
+  mutable struct ReluLayer
+    mask::BitArray
+  end
+  function ReluLayer(dim, N)
+    return ReluLayer(BitArray(zeros(dim, N)))
+  end
+  function forward(self::ReluLayer, x)
+    #println("x $(size(x)) mask $(size(self.mask))")
+    self.mask .= (x .<=  0)
+    out = copy(x)
+    out[self.mask] .= 0
+
+    return out
+  end
+  function backward(self::ReluLayer, dout)
+    dx = copy(dout)
+    dx[self.mask] .= 0
+
+    return dx
+  end
+
+  export SigmoidLayer
+  mutable struct SigmoidLayer
+    out::Array
+  end
+  function SigmoidLayer(dim)
+    return SigmoidLayer(zeros(dim, 1))
+  end
+  function forward(self::SigmoidLayer, x)
+    self.out = 1 ./ (1 .+ exp.(-x))
+
+    return self.out
+  end
+  function backward(self::SigmoidLayer, dout)
+    dx = dout .* (1.0 .- self.out) .* self.out
+
+    return dx
+  end
+
+
+  export AffineLayer
+  mutable struct AffineLayer
+    W::Array
+    b::Array
+    x::Array
+    dW::Array
+    db::Array
+  end
+  function AffineLayer(W, b, N)
+    return AffineLayer(W, b, zeros(size(W, 2), N), 0 .*W, 0 .*b)
+  end
+  function forward(self::AffineLayer, x)
+    self.x .= x
+
+    return self.W * x .+ self.b
+  end
+  function backward(self::AffineLayer, dout)
+    dx = self.W' * dout
+    self.dW .= dout * self.x'
+    #println("db $(size(self.db)) sum(dout) $(size(sum(dout, dims=2)))")
+    self.db .= vec(sum(dout, dims=2))
+
+    return dx
+  end
+
+  export SoftmaxWithLossLayer
+  mutable struct SoftmaxWithLossLayer
+    loss::Number
+    y::Array
+    t::Array
+    N::Number
+  end
+  function SoftmaxWithLossLayer(rowN, N)
+    return SoftmaxWithLossLayer(0, zeros(rowN, N), zeros(rowN, N), N)
+  end
+  function forward(self::SoftmaxWithLossLayer, x, t)
+    self.t .= t
+    self.y .= softmax(x, 1)
+    self.loss = cross_entropy_error(self.y, self.t)
+
+    return self.loss
+  end
+  function backward(self::SoftmaxWithLossLayer, dout)
+    dx = (self.y .- self.t) ./ self.N
+
+    return dx
+  end
+  function softmax(a, dims)
+    c = maximum(a)
+    return exp.(a .- c) ./ sum(exp.(a .- c), dims=dims)
+end
+  function cross_entropy_error(y, t)
+    batch_size = size(y, 2)
+    return -sum(t .* log.(y .+ 1e-7))/batch_size
+  end
+end
+
+# %% 5.4.1 乗算レイヤの実装
+using .BP
+
+apple = 100
+apple_num = 2
+tax = 1.1
+
+mul_apple_layer = BP.MulLayer()
+mul_tax_layer   = BP.MulLayer()
+
+apple_price = BP.forward(mul_apple_layer, apple, apple_num)
+price = BP.forward(mul_tax_layer, apple_price, tax)
+
+println(price)
+
+dprice = 1
+dapple_price, dtax = BP.backward(mul_tax_layer, dprice)
+dapple, dapple_num = BP.backward(mul_apple_layer, dapple_price)
+
+println(dapple)
+println(dapple_num)
+println(dtax)
+
+# %% 5.4.2 加算レイヤの実装
+using .BP
+
+apple = 100
+apple_num = 2
+orange = 150
+orange_num = 3
+tax = 1.1
+
+mul_apple_layer = BP.MulLayer()
+mul_orange_layer = BP.MulLayer()
+add_apple_orange_layer = BP.AddLayer()
+mul_tax_layer   = BP.MulLayer()
+
+apple_price = BP.forward(mul_apple_layer, apple, apple_num)
+orange_price = BP.forward(mul_orange_layer, orange, orange_num)
+all_price = BP.forward(add_apple_orange_layer, apple_price, orange_price)
+price = BP.forward(mul_tax_layer, all_price, tax)
+
+
+dprice = 1
+dall_price, dtax = BP.backward(mul_tax_layer, dprice)
+dapple_price, dorange_price = BP.backward(add_apple_orange_layer, dall_price)
+dorange, dorange_num = BP.backward(mul_orange_layer, dorange_price)
+dapple, dapple_num = BP.backward(mul_apple_layer, dapple_price)
+
+println(price)
+
+println(dapple)
+println(dapple_num)
+println(dorange)
+println(dorange_num)
+println(dtax)
+
+
+# %% 5.5 活性化関数レイヤの実装
+using .BP
+relu_layer = BP.ReluLayer(3, 2)
+
+input = [1 -1 0; -1 1 0.1]';
+BP.forward(relu_layer, input)
+BP.backward(relu_layer, ones(3, 2))
+
+
+sigmoid_layer = BP.SigmoidLayer(2)
+
+BP.forward(sigmoid_layer, input)
+BP.backward(sigmoid_layer, [1; 1])
+
+N = 2
+W = [0 0 0; 10 10 10]';
+b = [1 2 3]';
+affine_layer = BP.AffineLayer(W,b, N)
+
+x = [2 3; 4 5]'
+a = BP.forward(affine_layer, x)
+BP.backward(affine_layer, [1 2 3; 4 5 6]')
+
+
+t = [0 0 1; 0 1 0]';
+affine_layer = BP.SoftmaxWithLossLayer(3, N)
+BP.forward(affine_layer, a, t)
+BP.backward(affine_layer, [1 1 1; 1 1 1]')
+affine_layer.y
+affine_layer.t
+
+
+# %% 5.7 逆誤差伝播の実装
+module TLN2 # 2 Layer Net
+  using ..BP
+  layerOrder = ["Affine1" "Relu1" "Affine2"]
+
+  params = Dict()
+  layers = Dict()
+  grads_n = Dict()
+  grads_b = Dict()
+  o = Dict()
+
+  function init(input_size, hidden_size, output_size, N_, w_init_std = 0.01)
+    params["W1"] = w_init_std .* randn(input_size, hidden_size)'
+    params["b1"] = zeros(hidden_size)
+    params["W2"] = w_init_std .* randn(hidden_size, output_size)'
+    params["b2"] = zeros(output_size)
+
+    layers["Affine1"] = BP.AffineLayer(params["W1"], params["b1"], N_)
+    layers["Relu1"]   = BP.ReluLayer(hidden_size, N_)
+    layers["Affine2"] = BP.AffineLayer(params["W2"], params["b2"], N_)
+    o["lastLayer"] = BP.SoftmaxWithLossLayer(output_size, N_)
+    return;
+  end
+
+  function sigmoid(x)
+    return 1 ./ (1 .+ exp.(-x))
+  end
+
+  function softmax(a, dims)
+    c = maximum(a)
+    return exp.(a .- c) ./ sum(exp.(a .- c), dims=dims)
+  end
+
+  function predict(W1, W2, b1, b2, x)
+    a1 = W1*x .+ b1
+    z1 = max.(0, a1)
+    #print("$z1")
+    a2 = W2*z1 .+ b2
+    return softmax(a2, 1)
+  end
+
+  function predict(x)
+    W1, W2 = params["W1"], params["W2"]
+    b1, b2 = params["b1"], params["b2"]
+
+    return predict(W1, W2, b1, b2, x)
+  end
+
+  function predict2(x)
+    for layername in layerOrder
+      layer = layers[layername]
+      #print("$layername $(size(x))->")
+      x = BP.forward(layer, x)
+      #print("$(size(x))")
+      #if layername == "Relu1"
+      #  print("$x")
+      #end
+    end
+
+    return x
+  end
+
+  function loss(W1, W2, b1, b2, x, t)
+    y = predict(W1, W2, b1, b2, x)
+    return BP.cross_entropy_error(y, t)
+  end
+
+  function loss(x, t)
+    W1, W2 = params["W1"], params["W2"]
+    b1, b2 = params["b1"], params["b2"]
+    return loss(W1, W2, b1, b2, x, t)
+  end
+
+  function loss2(x, t)
+    y = predict2(x)
+    return BP.forward(o["lastLayer"], y, t)
+  end
+
+  function accuracy(x, t)
+    y = predict2(x)
+    y = map(r->r[1], argmax(y, dims=1))
+    t = map(r->r[1], argmax(t, dims=1))
+
+    return sum(y .== t) / size(x, 2)
+  end
+
+  function numerical_diff(f, x)
+    h = 1e-4
+    return (f(x+h)-f(x-h))/(2*h)
+  end
+
+  function numerical_gradient(f, x)
+    grad = 0*x;
+    for i in 1:length(x)
+      x_ = copy(x)
+      f_ = function(xvar)
+        x_[i] = xvar
+        return f(x_)
+      end
+      grad[i] = numerical_diff(f_, x[i])
+    end
+    return grad
+  end
+
+  function calc_grad(x, t)
+    W1, W2 = params["W1"], params["W2"]
+    b1, b2 = params["b1"], params["b2"]
+
+    grads_n["W1"] = numerical_gradient(w->loss(w, W2, b1, b2, x,t), W1)
+    grads_n["b1"] = numerical_gradient(w->loss(W1, W2, w, b2, x,t), b1)
+    grads_n["W2"] = numerical_gradient(w->loss(W1, w, b1, b2, x,t), W2)
+    grads_n["b2"] = numerical_gradient(w->loss(W1, W2, b1, w, x,t), b2)
+
+    return grads_n
+  end
+
+  function gradient(x, t)
+    loss2(x, t)
+
+    dout = ones(size(t))
+    dout = BP.backward(o["lastLayer"], dout)
+
+    for layername in reverse(layerOrder, dims=2)
+      #print("$layername $(size(dout))->")
+      layer = layers[layername]
+      dout = BP.backward(layer, dout)
+      #print("$(size(dout))")
+    end
+
+    grads_b["W1"] = layers["Affine1"].dW
+    grads_b["b1"] = layers["Affine1"].db
+    grads_b["W2"] = layers["Affine2"].dW
+    grads_b["b2"] = layers["Affine2"].db
+
+    return grads_b
+  end
+
+end
+
+# %% 5.7.3 誤差逆伝播の勾配確認
+include("./dataset/mnist.jl")
+using Statistics
+#%% load data
+(x_train, t_train), (x_test, t_test) = MNIST.load_mnist(one_hot_label=true, normalize=true);
+# %% main
+
+iters_num = 1
+batch_size = 2
+learning_rate = 0.1
+hid = 10
+
+TLN2.init(784, hid, 10, batch_size)
+
+x_batch = x_train[1:batch_size, :]';
+t_batch = t_train[1:batch_size, :]';
+
+TLN2.predict(x_batch)
+TLN2.softmax(TLN2.predict2(x_batch), 1)
+
+grad_numerical = TLN2.calc_grad(x_batch, t_batch)
+grad_backprop  = TLN2.gradient(x_batch, t_batch)
+
+for key in keys(grad_numerical)
+  diff = mean(grad_backprop[key] .- grad_numerical[key])
+  println("$key:$diff")
+end
+# %% 5.7.4 誤差逆伝播法を使った学習
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+train_size = size(x_train, 1)
+
+iters_num = 10000
+batch_size = 100
+learning_rate = 0.1
+hid = 50
+
+iter_per_epoch = Int(max(train_size/batch_size, 1))
+
+TLN2.init(784, hid, 10, batch_size)
+
+for i in 1:iters_num
+  batch_mask = rand(1:train_size, batch_size);
+  x_batch = x_train[batch_mask, :]';
+  t_batch = t_train[batch_mask, :]';
+
+  grad = TLN2.gradient(x_batch, t_batch)
+
+  for key in keys(grad)
+    TLN2.params[key] .-= learning_rate .* grad[key]
+    #                ↑これがないとBP内の構造体の重み変数が更新されない。参照型怖い
+  end
+
+  loss_ = TLN2.loss(x_batch, t_batch)
+  push!(train_loss_list, loss_)
+
+  if i % iter_per_epoch == 0
+    train_acc = TLN2.accuracy(x_train[1:batch_size, :]', t_train[1:batch_size, :]')
+    test_acc = TLN2.accuracy(x_test[1:batch_size, :]', t_test[1:batch_size, :]')
+    push!(train_acc_list, train_acc)
+    push!(test_acc_list, test_acc)
+    println("loss: $loss_")
+    println("train acc: $train_acc")
+    println("test acc: $test_acc")
+  end
+end
