c46e40e850b1db566fbf2edb7db49bbfbc47cad3
Mizuho <mizuho128@gmail.com>
2020-05-15 20:01:23 +0900

    Julia ch07 im2col convolution test using lena

--- julia/7_cnn.jl
+++ julia/7_cnn.jl
@@ -27,6 +27,15 @@ function im2col(input_data, filter_h, filter_w, stride=1, pad=0)
   return reshape(permutedims(col,(1,2,5,3,4)), N*out_h*out_w, :)
 end
 
+function Wx2im(Wx, OH, OW, FN, N)
+  return reshape(Wx, (OH, OW, FN, N))
+end
+
+function W2col(W)
+  FH,FW,C,FN = size(W)
+  return reshape(W, (FH*FW*C, FN))
+end
+
 # %% draft
 out_h = 2
 out_w = 2
@@ -42,9 +51,70 @@ col[4, :, :] .= img[2:3, 2:3];
 reshape(col, out_h*out_w, :)
 
 # %% test
-img2 = zeros(3,3, 2, 2)
+N = 2
+C = 2
+H = 3
+W = 3
+img2 = zeros(H,W, C, N)
 img2[:, :, 1,1] .= img
 img2[:, :, 2,1] .= img.+9
 img2[:, :, 1,2] .= -img
 img2[:, :, 2,2] .= -(img.+9)
-im2col(img2,2,2)
+
+col2 = im2col(img2,2,2)
+
+# %% 重み
+H = 3
+W = 3
+C = 1
+filter_h = 3
+filter_w = 3
+S = 1
+P = 1
+out_h = Int(floor((W+2*P-filter_h)/S)+1)
+out_w = Int(floor((W+2*P-filter_w)/S)+1)
+Wf = zeros(filter_h, filter_w, C, N);
+Wf[2, 2, 1, 1] = 1;
+#Wf[:, :, 2, 1] = 0.11 .* ones(3,3);
+#Wf[2, 2, 2, 1] = 0.12;
+Wf[:, :, 1, 2] = -0.11 .* ones(3,3);
+Wf[2, 2, 1, 2] = 1.88;
+#Wf[:, :, 2, 2] = [-1 0 1; -2 0 2; -1 0 1];
+Wf
+
+colWf = W2col(Wf);
+img3 = zeros(H,W, C, N);
+img3[:, :, 1, 1] = img;
+img3[:, :, 1, 2] = img .+ 9;
+img3
+col3 = im2col(img3,filter_h,filter_w, S,P)
+affine = col3*colWf
+
+
+# %%
+using Images, ImageIO
+using PyPlot
+
+lena = load("dataset/lena_gray.png");
+lenaAr = channelview(lena);
+#lenaF = Array{Float32, 2}(lenaAr[1, :, :]);
+imshow(lenaAr, cmap="gray")
+
+# %% conv lena
+H, W = size(lenaAr)
+FN = 4
+C = 1
+FH = 3
+FW = 3
+N = 1
+S = 1
+P = 1
+img4 = zeros(H,W, C, N);
+img4[:, :, 1, 1] = lenarAr
+Wf = zeros(filter_h, filter_w, C, N);
+Wf[2, 2, 1, 1] = 1;
+Wf[:, :, 1, 1] = 0.11 .* ones(3,3);
+Wf[2, 2, 1, 1] = 0.12;
+Wf[:, :, 1, 2] = -0.11 .* ones(3,3);
+Wf[2, 2, 1, 2] = 1.88;
+Wf[:, :, 1, 2] = [-1 0 1; -2 0 2; -1 0 1];
