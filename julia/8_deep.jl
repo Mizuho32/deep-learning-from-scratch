256927d95431b2d67052658e1ea9e35be60116f3
Mizuho <mizuho128@gmail.com>
2020-07-11 21:59:58 +0900

    Julia ch08 deepNet forward

---
+++ julia/8_deep.jl
@@ -0,0 +1,203 @@
+include("julia/lib8.jl"  )
+include("./dataset/mnist.jl")
+using PyCall
+using PyPlot
+pickle = pyimport("pickle")
+
+module DeepConvNet
+  layerOrder = ["Conv1", "Relu1", "Pool1", "Affine1", "Relu2", "Affine2"]
+
+  export DeepConvNet_st
+  mutable struct DeepConvNet_st
+    params::Dict{String, Array}
+    layers::Array{Any}
+    last_layer
+    grads::Dict{String, Array}
+    predict
+    loss
+    gradient
+    accuracy
+  end
+
+  struct ConvParam
+    filter_num::Number
+    filter_size::Number
+    pad::Number
+    stride::Number
+  end
+
+  function new(input_dim=(28, 28, 1), conv_params=[ConvParam(16, 3, 1, 1),ConvParam(16, 3, 1, 1),ConvParam(32, 3, 1, 1),ConvParam(32, 3, 2, 1),ConvParam(64, 3, 1, 1),ConvParam(64, 3, 1, 1),], hidden_size=50, output_size=10)
+    pre_node_nums = [[1*3*3] map(cp->cp.filter_num*cp.filter_size^2, conv_params)' [hidden_size]]
+    weight_init_scales = sqrt.(2.0 ./ pre_node_nums)
+
+    params = Dict{String, Array}()
+    pre_channel_num = input_dim[end]
+    for idx in 1:length(conv_params)
+      cp = conv_params[idx]
+      params["W$(idx)"] = weight_init_scales[idx] .* randn(cp.filter_size, cp.filter_size, pre_channel_num, cp.filter_num)
+      params["b$(idx)"] = zeros(1, cp.filter_num)
+      pre_channel_num = cp.filter_num
+    end
+
+    params["W7"] = weight_init_scales[7] .* randn(64*4*4, hidden_size)'
+    params["b7"] = zeros(hidden_size)
+    params["W8"] = weight_init_scales[8] .* randn(hidden_size, output_size)'
+    params["b8"] = zeros(output_size)
+
+    layers = []
+    for idx in 1:6
+      insert!(layers, length(layers)+1, Main.Convolution.new(params["W$(idx)"], params["b$(idx)"], conv_params[idx].stride, conv_params[idx].pad))
+      insert!(layers, length(layers)+1, Main.Relu.new())
+      if iszero(idx % 2)
+        insert!(layers, length(layers)+1, Main.Pooling.new(2, 2, 2))
+      end
+    end
+    insert!(layers, length(layers)+1, Main.Affine.new(params["W7"], params["b7"]))
+    insert!(layers, length(layers)+1, Main.Relu.new())
+    insert!(layers, length(layers)+1, Main.Dropout.new(0.5))
+    insert!(layers, length(layers)+1, Main.Affine.new(params["W8"], params["b8"]))
+    insert!(layers, length(layers)+1, Main.Dropout.new(0.5))
+
+
+    self = DeepConvNet_st(
+      params,
+      layers,
+      Main.SoftmaxWithLoss.new(),
+      Dict(
+        "W1" => [],
+        "b1" => [],
+        "W2" => [],
+        "W2" => [],
+        "W3" => [],
+        "b3" => []),
+     (x, t=false)->predict(self, x, t),
+     (x, t)->loss(self, x, t),
+     (x, t)->gradient(self, x, t),
+     (x, t, b=500)->accuracy(self, x, t, b))
+
+     return self
+  end
+
+  export load_param
+  function load_param(self, params)
+    Windices = [0 2 5 7 10 12 15 18] .+ 1
+    for i in 1:length(Windices)
+      layer_idx = Windices[i]
+      #println(typeof(self.layers[layer_idx]))
+      #println(layer_idx)
+      self.layers[layer_idx].W = params["W$(i)"]
+      self.layers[layer_idx].b = params["b$(i)"]
+    end
+  end
+
+  export load_pickle
+  function load_pickle(self, pdata)
+    newpdata = Dict{String,Array}()
+    for i in 1:8
+      wk = "W$(i)"
+      bk = "b$(i)"
+      w = pdata[wk]
+      b = pdata[bk]
+      if ndims(w) == 2
+        newpdata[wk] = w'
+        newpdata[bk] = reshape(b, (length(b), 1))
+      else
+        newpdata[wk] = permutedims(w, (3, 4, 2, 1))
+        newpdata[bk] = reshape(b, (1, length(b)))
+      end
+    end
+    load_param(self, newpdata)
+  end
+
+  function predict(self::DeepConvNet_st, x::Array, train_flg=false)
+    for layer in self.layers
+      #println(typeof(layer))
+      #print("$(size(x))->")
+      if isa(layer, Main.Dropout.Dropout_st)
+        x = layer.forward(x, train_flg)
+      else
+        x = layer.forward(x)
+      end
+      #println(size(x))
+    end
+    return x
+  end
+
+  function loss(self::DeepConvNet_st, x::Array, t)
+    y = self.predict(x, true)
+    return self.last_layer.forward(y, t)
+  end
+
+  function gradient(self::DeepConvNet_st, x::Array, t)
+    # forwad
+    self.loss(x, t)
+
+    # backward
+    dout = 1
+    dout = self.last_layer.backward(dout)
+
+    for layer in reverse(self.layers)
+      dout = layer.backward(dout)
+    end
+
+    Windices = [0 2 5 7 10 12 15 18] .+ 1
+    for i in 1:length(Windices)
+      layer_idx = Windices[i]
+      self.grads["W$(i)"] = self.layers[layer_idx].dW
+      self.grads["b$(i)"] = self.layers[layer_idx].db
+    end
+    return self.grads
+  end
+
+  function accuracy(self::DeepConvNet_st, x, t, batch_size=500)
+    t = argmax.(eachrow(t))
+    acc = 0.0
+
+    for i in 1:(Int(size(x, 4) / batch_size))
+      tx = x[:, :, :, (i-1)*batch_size+1:i*batch_size]
+      tt = t[(i-1)*batch_size+1:i*batch_size]'
+      y = self.predict(tx)
+      y = reshape(argmax.(eachcol(y)), (1, size(y, 2)))
+      acc += sum(y .== tt)
+    end
+
+    return acc / size(x, 4)
+  end
+end
+function unpickle(filename)
+    r = nothing
+    @pywith pybuiltin("open")(filename,"rb") as f begin
+        r = pickle.load(f)
+    end
+    return r
+end
+
+# %% Inference
+network = DeepConvNet.new();
+
+# %% load pickle
+pdata = unpickle("./ch08/deep_convnet_params.pkl");
+DeepConvNet.load_pickle(network, pdata)
+tmp = "W8"
+size(pdata[tmp])#[1:5, 1:5]
+size(network.layers[19].W)#.W[1:5, 1:5]
+
+# %% Load
+(x_train, t_train), (x_test, t_test) = MNIST.load_mnist(one_hot_label=true, flatten=false);
+train_size = size(x_train, 4)
+
+# %% test
+size(network.layers[16].W)
+n = 2
+argmax(t_test[n, :])-1
+size(network.layers[16].W)
+size(network.layers[16].b)
+size(network.layers[16].forward(ones(4,4,64,1)))
+argmax(network.predict(x_test[:, :, :, n:n]))[1]-1
+imshow(x_test[:, :, 1, n])
+
+# %% main
+
+# %%
+weight = unpickle("./ch08/deep_convnet_params.pkl")
+size(weight["b7"])
---
+++ julia/lib8.jl
@@ -0,0 +1,324 @@
+module Relu
+  export Relu_st
+  mutable struct Relu_st
+    mask::Array
+    forward
+    backward
+  end
+  function new()
+    self = Relu_st(BitArray(zeros(0, 0)), (x)->forward(self, x), (d)->backward(self, d))
+    return self
+  end
+  function forward(self::Relu_st, x)
+    self.mask = (x .<=  0)
+    out = copy(x)
+    out[self.mask] .= 0
+
+    return out
+  end
+  function backward(self::Relu_st, dout)
+    dx = copy(dout)
+    dx[self.mask] .= 0
+
+    return dx
+  end
+end
+
+module Dropout
+  export Dropout_st
+  mutable struct Dropout_st
+    dropout_ratio
+    mask::Array
+    forward
+    backward
+  end
+  function new(dropout_ratio=0.5)
+    self = Dropout_st(dropout_ratio, [], (x, t=true)->forward(self, x, t), (d)->backward(self, d))
+    return self
+  end
+  function forward(self::Dropout_st, x, train_flg=true)
+    if train_flg
+      self.mask = rand(Float64, size(x)) .> self.dropout_ratio
+      return x .* self.mask
+    else
+      return x .* (1.0 - self.dropout_ratio)
+    end
+  end
+  function backward(self::Dropout_st, dout)
+    return dout .* self.mask
+  end
+end
+
+module Sigmoid
+  export Sigmoid_st
+  mutable struct Sigmoid_st
+    out::Array
+    forward
+    backward
+  end
+  function new(dim)
+    self = Sigmoid_st(zeros(dim, 1), (x)->forward(self, x), (d)->backward(self, d))
+    return self
+  end
+  function forward(self::Sigmoid_st, x)
+    self.out = 1 ./ (1 .+ exp.(-x))
+
+    return self.out
+  end
+  function backward(self::Sigmoid_st, dout)
+    dx = dout .* (1.0 .- self.out) .* self.out
+
+    return dx
+  end
+end
+
+module Affine
+  export Affine_st
+  mutable struct Affine_st
+    W::Array
+    b::Array
+    x::Array
+    original_x_shape::Tuple
+    dW::Array
+    db::Array
+    forward
+    backward
+  end
+  function new(W, b)
+    self = Affine_st(W, b, zeros(size(W, 2), 0), (), 0 .*W, 0 .*b, (x)->forward(self, x), (d)->backward(self, d))
+    return self
+  end
+  function forward(self::Affine_st, x)
+    self.original_x_shape = size(x)
+    x = reshape(x, (:, self.original_x_shape[end]))
+    self.x = x
+
+    return self.W * x .+ self.b
+  end
+  function backward(self::Affine_st, dout)
+    dx = self.W' * dout
+    self.dW .= dout * self.x'
+    self.db .= vec(sum(dout, dims=2))
+
+    return reshape(dx, self.original_x_shape)
+  end
+end
+
+module SoftmaxWithLoss
+  export SoftmaxWithLoss_st
+  mutable struct SoftmaxWithLoss_st
+    loss::Number
+    y::Array
+    t::Array
+    forward
+    backward
+  end
+  function new()
+    self = SoftmaxWithLoss_st(0, zeros(0, 0), zeros(0, 0), (x, t)->forward(self, x, t), (d)->backward(self, d))
+    return self
+  end
+  function forward(self::SoftmaxWithLoss_st, x, t)
+    self.t = t
+    self.y = softmax(x, 1)
+    self.loss = cross_entropy_error(self.y, self.t)
+
+    return self.loss
+  end
+  function backward(self::SoftmaxWithLoss_st, dout)
+    dx = (self.y .- self.t) ./ size(dout, 2)
+
+    return dx
+  end
+  function softmax(a, dims)
+    c = maximum(a)
+    return exp.(a .- c) ./ sum(exp.(a .- c), dims=dims)
+  end
+  function cross_entropy_error(y, t)
+    batch_size = size(y, 2)
+    return -sum(t .* log.(y .+ 1e-7))/batch_size
+  end
+end
+
+function im2col(input_data, filter_h, filter_w, stride=1, pad=0)
+  H, W, C, N = size(input_data)
+  out_h = Int(floor((H + 2*pad - filter_h)/stride + 1))
+  out_w = Int(floor((W + 2*pad - filter_w)/stride + 1))
+
+  img = input_data
+  if pad != 0
+    img = zeros(H+2*pad, W+2*pad, C, N)
+    img[(1+pad):(H+pad), (1+pad):(W+pad), :, :] .= input_data
+  end
+  col = zeros(out_h, out_w, filter_h*filter_w, C, N)
+
+  col_idx = 1
+  for x in 1:filter_w
+    x_max = x + stride*(out_w-1)
+    for y in 1:filter_h
+      y_max = y + stride*(out_h-1)
+      col[:, :, col_idx, :, :] = img[y:stride:y_max, x:stride:x_max, :, :]
+      col_idx+=1
+    end
+  end
+
+  return reshape(permutedims(col,(1,2,5,3,4)), N*out_h*out_w, :)
+end
+
+function col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0)
+  H, W, C, N = input_shape
+  out_h = Int(floor((H + 2*pad - filter_h)/stride + 1))
+  out_w = Int(floor((W + 2*pad - filter_w)/stride + 1))
+  col = permutedims(reshape(col, (out_h, out_w, N, filter_h*filter_w, C)), (1,2,4,5,3))
+
+  img = zeros(H + 2*pad + stride - 1, W + 2*pad + stride - 1, C, N)
+  col_idx = 1
+  for x in 1:filter_w
+    x_max = x + stride*(out_w-1)
+    for y in 1:filter_h
+      y_max = y + stride*(out_h-1)
+      img[y:stride:y_max, x:stride:x_max, :, :] .+= col[:, :, col_idx, :, :]
+      col_idx+=1
+    end
+  end
+
+  return img[(pad+1):(H+pad), (pad+1):(W+pad), :, :]
+end
+
+function Wx2im(Wx, OH, OW, FN, N)
+  return permutedims(reshape(Wx, (OH, OW, N, FN)), (1, 2, 4, 3))
+end
+
+function im2Wx(im, FN)
+  return reshape(permutedims(im, (1, 2, 4, 3)), (:, FN))
+end
+
+function W2col(W)
+  FH,FW,C,FN = size(W)
+  return reshape(W, (FH*FW*C, FN))
+end
+
+function col2W(colW, FH, FW, C, FN)
+  return reshape(colW, (FH, FW, C, FN))
+end
+
+module Convolution
+  mutable struct Convolution_st
+    W::Array
+    b::Array
+    stride::Number
+    pad::Number
+    x::Array
+    col::Array
+    col_W::Array
+    db::Array
+    dW::Array
+    forward
+    backward
+  end
+
+  function new(W, b, stride=1, pad=0)
+    tmp_st = Convolution_st(W, b, stride, pad,  [0], [0], [0], [0], [0], (x)->forward(tmp_st, x), (x)->backward(tmp_st, x))
+    return tmp_st
+  end
+
+  function forward(self::Convolution_st, x)
+    FH, FW, C, FN = size(self.W)
+    H, W, C, N = size(x)
+    out_h = Int(floor((H + 2*self.pad - FH)/self.stride + 1))
+    out_w = Int(floor((W + 2*self.pad - FW)/self.stride + 1))
+
+    col = Main.im2col(x, FH, FW, self.stride, self.pad)
+    col_W = Main.W2col(self.W)
+
+    self.x = x;
+    self.col = col
+    self.col_W = col_W
+
+    return Main.Wx2im(col*col_W .+ self.b, out_h, out_w, FN, N)
+  end
+
+  function backward(self::Convolution_st, dout)
+    FH, FW, C, FN = size(self.W)
+    dout = Main.im2Wx(dout, FN)
+
+    self.db = sum(dout, dims=1)
+    self.dW = Main.col2W(self.col' * dout, FH, FW, C, FN)
+
+    dcol = dout*self.col_W'
+    return Main.col2im(dcol, size(self.x), FH, FW, self.stride, self.pad)
+  end
+end
+
+module Pooling
+  mutable struct Pooling_st
+    x::Array
+    arg_max::Array
+    pool_h::Number
+    pool_w::Number
+    out_h::Number
+    out_w::Number
+    stride::Number
+    pad::Number
+    forward
+    backward
+  end
+
+  function new(pool_h, pool_w, stride=1, pad=0)
+    self = Pooling_st([], [], pool_h, pool_w, 0, 0, stride, pad, (x)->forward(self, x), (dout)->backward(self, dout))
+    return self
+  end
+
+  function forward(self::Pooling_st, x)
+    H, W, C, N = size(x)
+    self.out_h = Int(floor((H + 2*self.pad - self.pool_h)/self.stride + 1))
+    self.out_w = Int(floor((W + 2*self.pad - self.pool_w)/self.stride + 1))
+
+    col = reshape(Main.im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)', (self.pool_h*self.pool_w, :))
+
+    self.x = x
+    self.arg_max = (argmax.(eachcol(col))' .== 1:size(col, 1))
+    return permutedims(reshape(maximum(col, dims=1), (C, self.out_h, self.out_w, N)), (2, 3, 1, 4))
+  end
+
+  function backward(self::Pooling_st, dout)
+    sizex = size(self.x)
+    H, W, C, N = sizex
+    pool_hw = self.pool_h*self.pool_w
+    out_hw  = self.out_h*self.out_w
+
+    dout = reshape(dout, (out_hw, 1, C, N))
+    W = permutedims(reshape(self.arg_max, (pool_hw, C, out_hw, N)), (3,1,2,4))
+
+    dcol = reshape(permutedims(dout .* W, (1,4,2,3)), (:, pool_hw*C))
+    return Main.col2im(dcol, sizex, self.pool_h, self.pool_w, self.stride, self.pad)
+  end
+
+end
+
+module AdaGrad
+  mutable struct SGD_st
+    lr
+    h
+    update
+  end
+
+  function new(lr=0.01)
+    tmp_st = SGD_st(lr, nothing, nothing)
+    tmp_st.update = (p,g)->update(tmp_st, p, g)
+    return return tmp_st
+  end
+
+  function update(self::SGD_st, params::Dict, grads::Dict)
+    if self.h == nothing
+      self.h = Dict()
+      for key in keys(params)
+        self.h[key] = zeros(size(params[key]))
+      end
+    end
+
+    for key in keys(params)
+      self.h[key] += grads[key] .* grads[key]
+      params[key] .-= self.lr .* grads[key] ./ (sqrt.(self.h[key]) .+ 1.e-7)
+    end
+  end
+end
